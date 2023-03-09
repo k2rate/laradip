da0477d903b89eda51e3a564a1e2e3115aacc499
k2rate <dns.1983n@gmail.com>
2023-03-09 02:03:00 -0800

    redirect

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -10,7 +10,7 @@ class AboutController extends Controller
     public function index()
     {
         $products = Product::orderBy('id', 'asc')->limit(5)->get(); // the oldest entry
-        
+
         return view('about', compact('products'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('main');
+    return redirect()->to('/about');
 });
 
 Auth::routes();
